acd7d670de5f566b65fa0e6757fe16d85cde6b91
rohansinghwow <rohansinghwow@gmail.com>
2021-12-24 16:48:41 +0530

    adding components

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,52 @@
 import React from "react";
+import Place from './Place'
+import './main.css'
+export default function App(){
 
-export default () => (
-  <>
-    <h1>DDD</h1>
-    <p>Hard to get more minimal than this React app.</p>
-  </>
-);
+  const [state, setState] = React.useState([])
+  const [loading, setLoading] = React.useState(true)
+    
+
+    const mapper = state.map(item=>{  //maping the state array and send it to Places.jsx
+        
+        return (
+            
+                <Place name={item.name} img={item.image} info={item.info} 
+                price={item.price}
+                />
+            
+            
+        )
+        
+    })
+
+    function getData(){
+        const url = 'https://course-api.com/react-tours-project'
+        fetch(url)
+        .then(res=>res.json())
+        .then(data=>{
+            setState(data)
+            setLoading(false)
+        })
+        
+        
+      }
+    
+    React.useEffect(()=>{
+        getData() //get API data after rendering
+        
+        
+    }, [])
+
+return (
+        
+    loading? <h1>Loading...</h1> : mapper
+        
+)
+
+  
+}
+
+
+
+
---
+++ src/Place.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Place(props){
+    return(
+        <div className="card">
+            <div className="card-img">
+                <img src={props.img} />
+            </div>
+            
+            <p className="card-name">{props.name}</p>
+            <p>{props.info}</p>
+            <p className="card-price">{props.price}</p>
+        </div>
+    )
+}
