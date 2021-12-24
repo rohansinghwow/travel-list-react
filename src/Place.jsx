2b529fce3bf8a70e9a31d0db3efc002c6e26bce9
rohansinghwow <rohansinghwow@gmail.com>
2021-12-24 18:00:38 +0530

    Adding readmore feature

--- src/Place.jsx
+++ src/Place.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 
 export default function Place(props){
+
+    const [readMore, setReadMore] = React.useState(false)
     return(
         <div className="card">
             <div className="card-img">
@@ -8,7 +10,9 @@ export default function Place(props){
             </div>
             
             <p className="card-name">{props.name}</p>
-            <p className="card-info">{props.info}</p>
+            <p className="card-info">{readMore?props.info:props.info.substring(0,274)}
+                <button className="readmore-btn" onClick={()=>setReadMore(prevData=>!prevData)}>Read More</button>
+            </p>
             <p className="card-price">${props.price}</p>
         </div>
     )
